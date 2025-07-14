24b6eb822b3c54c641d114306b0e20e000f50f73
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-07-14 22:39:52 +0300

    задача 2.6.12 первая проверка

---
+++ src/FindMinMax.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FindMinMax {
+    public <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> list = stream.sorted(order).collect(Collectors.toList());
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+        FindMinMax fMM = new FindMinMax();
+        BiConsumer<Integer, Integer> consumer = (min, max) -> System.out.println(min + "-" + max);
+
+        fMM.findMinMax(list.stream(), Integer::compareTo, consumer);
+    }
+}
